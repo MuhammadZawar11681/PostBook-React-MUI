afa3d8eecc19f46732048dda71d144a0b47b27b2
Muhammad Zawar <mzawar189@gmail.com>
2024-08-16 14:25:15 +0500

    Add new feature

--- src/components/Feed.jsx
+++ src/components/Feed.jsx
@@ -1,7 +1,9 @@
 import { Box, Stack, Skeleton } from "@mui/material";
 import React, { useState } from "react";
 import Post from "./Post";
-import image from "../images/StockCake-Excited Programmer Working_1723790801.jpg";
+import image1 from "../images/StockCake-Excited Programmer Working_1723790801.jpg";
+import image2 from "../images/AdobeStock_776380075_Preview.jpeg";
+import image3 from "../images/altumcode-PNbDkQ2DDgM-unsplash.jpg";
 
 const Feed = ({ posts }) => {
   const [loading, setLoading] = useState(true);
@@ -24,7 +26,9 @@ const Feed = ({ posts }) => {
           {posts.map((post, index) => (
             <Post key={index} text={post.text} image={post.image} />
           ))}
-          <Post text={"Excited Programmer Working"} image={image} />
+          <Post text={"Excited Programmer Working"} image={image1} />
+          <Post text={"Let Break Time 🍵"} image={image2} />
+          <Post text={"Coding is Fun"} image={image3} />
         </>
       )}
     </Box>
